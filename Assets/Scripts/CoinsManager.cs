4c0a212473428b69d65287e468f861377408c82a
Felipexox <felipeaa100@gmail.com>
2018-07-03 11:43:58 -0300

    [Commit]   - Added background and new mechanic of points with coins

---
+++ Assets/Scripts/CoinsManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinsManager : MonoBehaviour {
+    private static CoinsManager instance;
+    private int maxCoinLevel = 0;
+    private int currentCoinLevel = 0;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        maxCoinLevel = Object.FindObjectsOfType<Coin>().Length;
+    }
+    public int GetCoinsTotal
+    {
+        get {
+            if (currentCoinLevel > maxCoinLevel)
+                return maxCoinLevel;
+            return maxCoinLevel;
+        }
+    }
+    public void AddCoin()
+    {
+        currentCoinLevel++;
+    }
+
+    public static CoinsManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+}
